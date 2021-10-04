a4a16c8c16f1315776c6127319cb96e5190cd99c
Antonio Ignacio Cambrils Oruña <antonio.cambrils@semicrol.com>
2021-10-04 20:48:53 +0200

    4/10/21 Version 1.0

---
+++ Scheduler.Bussiness/ScheduleConfig.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler
+{
+    public abstract class ScheduleConfig
+    {
+        public ScheduleConfig() { }
+
+        public static abstract ScheduleEvent ScheduleNextExecution(DateTime CurrentDate) { }
+
+        public ScheduleType Type { get; private set; }
+        public string ExecutionDescription { get; private set; }
+    }
+
+    public class ScheduleConfigOnce : ScheduleConfig
+    {
+
+    }
+
+    public class ScheduleConfigRecurring : ScheduleConfig
+    {
+
+    }
+
+    public class Scheduler
+    {
+        public Scheduler() { }
+
+        public ScheduleConfig SetConfig (ScheduleType Type, OccurrencyPeriod )
+    }
+
+    public enum ScheduleType
+    {
+        Once,
+        Recurring
+    }
+
+    public enum OccurrencyPeriod
+    {
+        Daily = 1,
+        Weekly = 7,
+        Monthly,
+        Yearly
+    }
+}
---
+++ Scheduler.Bussiness/ScheduleEvent.cs
@@ -0,0 +1,40 @@
+﻿using Scheduler.Resources;
+using System;
+
+namespace Scheduler
+{
+    public class ScheduleEvent
+    {
+        public ScheduleEvent(DateTime ExecutionTime, ScheduleType ExecutionType, DateTime StartDate)
+        {
+            ValidateDate(StartDate);
+            ValidateDate(ExecutionTime);
+
+            this.ExecutionTime = ExecutionTime;
+            this.ExecutionType = ExecutionType;
+            this.ExecutionStartDate = StartDate;
+            this.FormatEventDescription();
+            
+        }
+
+        public DateTime ExecutionTime { get; private set; }
+        public ScheduleType ExecutionType { get; private set; }
+        public DateTime ExecutionStartDate { get; private set; }
+        public string ExecutionDescription { get; private set; }
+
+        private static void ValidateDate (DateTime Date)
+        {
+            if (string.IsNullOrWhiteSpace(Date.ToString()))
+            {
+                throw new Exception(TextResources.ExcDate);
+            }
+        }
+
+        private void FormatEventDescription()
+        {
+            string ExectionDate = this.ExecutionTime.ToString("d");
+            string ExecutionHour = this.ExecutionTime.ToString("t");
+            this.ExecutionDescription = string.Format(TextResources.EventScheduleDescription, this.ExecutionType.ToString(), ExectionDate, ExecutionHour, this.ExecutionStartDate.ToString("d"));
+        }
+    }
+}
